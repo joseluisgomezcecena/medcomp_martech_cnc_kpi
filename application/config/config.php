5e4bc94f62cc4eb0061cf940c224c1aa5ee8deaf
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-08-16 16:37:28 -0700

    Updated base_url on config.php for production server.

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ date_default_timezone_set('America/Tijuana');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cnc_kpi';
+$config['base_url'] = 'http://mxmtsvrandon1/cnc';
 
 /*
 |--------------------------------------------------------------------------
