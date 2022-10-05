42f9e16f6c63573fb0e735650c562a697a09c42b
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-10-05 16:01:22 -0700

    Updated Login views.

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,33 +1,155 @@
-<div class="container flex items-center justify-center mt-20 py-10">
-	<div class="w-full md:w-1/2 xl:w-1/3">
-		<div class="mx-5 md:mx-10">
-			<h2 class="uppercase">It’s Great To See You!</h2>
-			<h4 class="uppercase">Login Here</h4>
-		</div>
-		<div class="card mt-5 p-5 md:p-10">
-
-			<?php echo form_open('users/login') ?>
-				<div class="mb-5">
-					<label class="label block mb-2" for="email">UserName</label>
-					<input id="email" type="text" name="username" class="form-control" placeholder="example@example.com">
-				</div>
-				<div class="mb-5">
-					<label class="label block mb-2" for="password">Password</label>
-					<label class="form-control-addon-within">
-						<input id="password" type="password" name="password" class="form-control border-none" value="12345">
-						<span class="flex items-center ltr:pr-4 rtl:pl-4">
-                            <button type="button"
-									class="btn btn-link text-gray-300 dark:text-gray-700 la la-eye text-xl leading-none"
-									data-toggle="password-visibility"></button>
-                        </span>
-					</label>
-				</div>
-				<div class="flex items-center">
-					<a href="auth-forgot-password.html" class="text-sm uppercase">Forgot Password?</a>
-					<button type="submit" class="btn btn_primary ltr:ml-auto rtl:mr-auto uppercase">Login</button>
-				</div>
-			</form>
-
+<div class="page-breadcrumb bg-white">
+	<div class="row align-items-center">
+		<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+			<h4 class="page-title">Protected Area</h4>
 		</div>
 	</div>
 </div>
+
+
+
+
+<div id="pendingdiv" class="container-fluid">
+	<!-- ============================================================== -->
+	<!-- Three charts -->
+	<!-- ============================================================== -->
+	<div class="row justify-content-center">
+
+		<div class="col-lg-12">
+			<div class="white-box analytics-info">
+				<h3 class="box-title">Please login to continue.</h3>
+
+				<?php echo form_open(base_url() . 'users/login')?>
+
+
+
+				<div class="row mt-5">
+					<div class="col-3">
+						<label for="">User Name</label>
+						<input id="pph" name="username" type="text" class="form-control">
+					</div>
+					<div class="col-3">
+						<label for="">Password</label>
+						<input id="password" name="password" type="password" class="form-control">
+					</div>
+
+					<div class="col-12 mt-5">
+						<input type="submit" class="btn btn-outline-primary" value="Login">
+					</div>
+
+				</div>
+
+
+				<?php echo form_close()?>
+
+			</div>
+		</div>
+
+
+
+	</div>
+</div>
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"></script>
+
+<script>
+
+
+	function searchnow() {
+
+		var plant_id = $("#pn").val();
+
+		alert("works");
+		// Ajax request
+		$.ajax({
+			url: '<?=base_url()?>index.php/ProductionForms/get_insert_details',
+			method: 'post',
+			data: {plant_id: plant_id},
+			dataType: 'json',
+			success: function (response) {
+				$.each(response, function (index, data) {
+
+					alert("Parts Per Hour: "+data['COL4']);
+					$('#pph').val(data['COL4']);
+				});
+			}
+		});
+	}
+
+
+
+	document.querySelector("#end-time").addEventListener("change", myFunction);
+
+	function myFunction() {
+
+		var pph = $('#pph').val();
+
+		//value start
+		var start = Date.parse($("input#start-time").val()); //get timestamp
+
+		//value end
+		var end = Date.parse($("input#end-time").val()); //get timestamp
+
+		totalHours = NaN;
+		goal = NaN;
+
+		//miliseconds = end - start;
+		//alert(miliseconds);
+		if (start < end) {
+
+
+			totalHours = Math.round((end - start) / 1000 / 60 / 60 ); //milliseconds: /1000 / 60 / 60
+			goal = totalHours*pph;
+		}else{
+			alert("Start date must be before end date. Check your dates.");
+		}
+
+		$("#total-hours").val(totalHours);
+		$("#goal").val(goal);
+
+	}
+</script>
+
+<script>
+	$('#entries-list').DataTable({
+		'scrollX': true,
+		"oLanguage": {
+			"sEmptyTable": "No se encontro... <a href='<?php echo base_url() ?>request/new' class='btn btn-primary'>Agregar Aqui</a>",
+			"sZeroRecords": "No se encontro... <a href='<?php echo base_url() ?>request/new' class='btn btn-primary'>Agregar Aqui</a>"
+		}
+		//'bSort': false,
+		//'scrollCollapse': true,
+	});
+	/*karen bora alexc rox luigi carlos pollo
+	$('#entries-list').DataTable({
+		'scrollX': true,
+		//'bSort': false,
+		//'scrollCollapse': true,
+
+		'processing': true,
+		'serverSide': true,
+		'serverMethod': 'post',
+		'ajax': {
+		'url': '<?php echo base_url() ?>datatables/home_calidad.php'
+		},
+		'columns': [
+			{ data: 'id' },
+			{ data: 'part_no' },
+			{ data: 'lot_no' },
+			{ data: 'qty' },
+			{ data: 'planta' },
+			{ data: 'progress' },
+			{ data: 'btn_id' },
+		]
+	});
+	*/
+</script>
+
+
